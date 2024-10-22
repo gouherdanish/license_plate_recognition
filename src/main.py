44aaea8778006a70477aa37e67d3b82519e7e52b
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-10-22 11:14:51 +0530

    modified main.py file

--- src/main.py
+++ src/main.py
@@ -2,12 +2,19 @@ import argparse
 import pytesseract
 import cv2
 
+def read(img_path):
+    img = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
+    return img
+
 def ocr(img_path):
-    pass
+    img = read(img_path)
+    extract = pytesseract.image_to_string(img)
+    return extract
 
 if __name__=='__main__':
     argparser = argparse.ArgumentParser(description="License Plate Recognition via Tesseract")
-    argparser.add_argument(name='--img_path',type='str',required=True,help='Image path to be fed to OCR')
+    argparser.add_argument('--img_path',type=str,required=True,help='Image path to be fed to OCR')
 
     args = argparser.parse_args()
-    ocr(vars(*args))
+    text = ocr(**vars(args))
+    print(text)
